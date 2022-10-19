a6e209f058cb2801dc3d0ad16ba1a255ce7181b3
Ben Allen <benalleng@gmail.com>
2022-10-19 15:02:01 -0400

    wip: add auth

--- main_app/models.py
+++ main_app/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class Package(models.Model):
     name = models.CharField(max_length=100)
-    
+    description = models.CharField(max_length=250)
+    user = models.ForeignKey(User)
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import DetailView, ListView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -8,3 +15,17 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def signup(request):
+    error_message = None
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = 'Signup Input Invalid - Please try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
