748a039a1bb622804b6bd7ffc887b02382f273b2
Ben Allen <benalleng@gmail.com>
2022-10-19 23:12:14 -0400

    add workflow model

--- main_app/views.py
+++ main_app/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Package
+from .models import Package, Workflow
 from .forms import WorkflowForm
 
 # Create your views here.
